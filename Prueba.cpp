be126f3e324159af59ca6eca38c9441ed5f25841
edu20056 <ejcanessaq@gmail.com>
2025-03-12 08:42:01 -0600

    Funcion de resta, inicializacion de static int para ver valores de memoria.

--- Prueba.cpp
+++ Prueba.cpp
@@ -6,16 +6,31 @@
 // & retrornar la posicion de variable
 // * acceder a variable
 
-int sumaDeNumeroYPuntero(int num1, int num2)
+int sumaDeNumerosYPuntero(int num1, int num2)
 {
-    int num3 = num1 + num2;
+    static int num3 = num1 + num2;
     int* ptr = &num3;
     printf("Se ingresaron los valores %d y %d, el puntero de la suma es %p", num1, num2, ptr);
     return 0;
 }
 
+
+int* restaDeNUmerosYPuntero(int num1, int num2)
+{
+    static int num3 = num1 - num2;
+    int* ptr = &num3;
+    printf("Se ingresaron los valores %d y %d, la resta es %d, su puntero es %p", num1, num2, num3, ptr);
+    return ptr;
+}
 int main()
 {
-    sumaDeNumeroYPuntero(12,5);
+    sumaDeNumerosYPuntero(12,5);
+    printf("\n");
+    int* ptr = restaDeNUmerosYPuntero(12, 2);
+    printf("\n");
+    printf("\n");
+    printf("El puntero de resta apunta a %d con valor de memoria de %p", *ptr, ptr);
+    printf("\n");
+
     return 0;
 }
